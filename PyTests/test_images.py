1291b1e284634604b8554b297449be7ce3e61b82
Yahya Al-Shamali <42281247+PointlessUsername@users.noreply.github.com>
2023-05-24 01:12:34 -0600

    Update test_images.py

--- PyTests/test_images.py
+++ PyTests/test_images.py
@@ -78,7 +78,7 @@ def test_print_img():
 
 # returns a base64 encoded image
 def get_input():
-    return getInputImg("coconut.jpg")
+    return getInputImg(os.path.join(THIS_DIR, "coconut.jpg"))
 
 
 # returns a base64 encoded image
