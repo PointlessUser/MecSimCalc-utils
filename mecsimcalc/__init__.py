51732367e45aa05728b927d8f9e38d05435da725
Yahya Al-Shamali <yaalshama@gmail.com>
2024-07-12 10:40:50 -0600

    Fix import error

--- mecsimcalc/__init__.py
+++ mecsimcalc/__init__.py
@@ -1,32 +1,32 @@
 
 # included in __all__: import using "from mecsimcalc import *" or "import mecsimcalc"
 
-from mecsimcalc.file_utils.general_utils import input_to_file, metadata_to_filetype
+from .file_utils.general_utils import input_to_file, metadata_to_filetype
 
-from mecsimcalc.file_utils.image_utils import input_to_PIL, file_to_PIL, print_image
+from .file_utils.image_utils import input_to_PIL, file_to_PIL, print_image
 
 
-from mecsimcalc.file_utils.plotting_utils import (
+from .file_utils.plotting_utils import (
     print_plot,
     print_animation,
     animate_plot,
 )
 
 
-from mecsimcalc.file_utils.spreadsheet_utils import (
+from .file_utils.spreadsheet_utils import (
     input_to_dataframe,
     file_to_dataframe,
     print_dataframe,
 )
 
-from mecsimcalc.file_utils.table_utils import table_to_dataframe, print_table
+from .file_utils.table_utils import table_to_dataframe, print_table
 
-from mecsimcalc.file_utils.text_utils import string_to_file
+from .file_utils.text_utils import string_to_file
 
-from mecsimcalc.file_utils.quiz_utils import append_to_google_sheet, send_gmail
+from .file_utils.quiz_utils import append_to_google_sheet, send_gmail
 
 # not included in __all__: import using "from mecsimcalc.plot_draw import *" or "import mecsimcalc.plot_draw"
-from mecsimcalc.eng130.plot_draw import *
+from .plot_draw import *
 
 
 __all__ = [
